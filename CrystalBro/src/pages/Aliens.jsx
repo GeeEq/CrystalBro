0cc1f921c8862b5d2e54a3e8829d32f95ea41cf7
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-06-20 19:45:55 +0300

    dracons list styled

--- CrystalBro/src/pages/Aliens.jsx
+++ CrystalBro/src/pages/Aliens.jsx
@@ -1,6 +1,7 @@
 import { useData } from "../backend/FetchData";
 import { Link, useParams } from "react-router-dom";
 import "./Aliens.css";
+import { Spacer } from "./Spacer";
 
 export function Aliens() {
   const aliens = useData("aliens");
@@ -15,11 +16,23 @@ export function Aliens() {
     aliens && (
       <>
         <div className="aliensListAddBar">
-          <h1 className="aliensList">Aliens List</h1>
+          <div className="listAddWrapper">
+            <h1 className="aliensList">Aliens</h1>
+          </div>
+          <Spacer />
+          <p className="descriptionText">
+            An alien is an extraterrestrial being from another world, typically
+            described as having a physical appearance, abilities, and behavior
+            that is very different from that of a human. Aliens often play a
+            role in science fiction stories and can be viewed as either
+            beneficial or hostile to humans.
+          </p>
+          <Spacer className="spacer" />
+
           <button className="addAlien">
-            <Link to={"/addAliens"} className="link">
-              ADD ALIEN
-            </Link>
+            {/* <Link to={"/addAliens"} className="link"> */}
+            ADD ALIEN
+            {/* </Link> */}
           </button>
         </div>
 
@@ -30,9 +43,18 @@ export function Aliens() {
                 <h3>{item.name}</h3>
                 <img src={item.imgUrl} className="imgURL" />
 
-                <p>Type: {item.type}</p>
-                <p>Description: {item.description}</p>
-                <p>Habitat: {item.habitat}</p>
+                <p>
+                  <span>Type: </span>
+                  {item.type}
+                </p>
+                <p>
+                  <span>Description: </span>
+                  {item.description}
+                </p>
+                <p>
+                  <span>Habitat: </span>
+                  {item.habitat}
+                </p>
                 <div className="btn">
                   <button className="delete">Delete</button>
                 </div>
--- CrystalBro/src/pages/Dragons.jsx
+++ CrystalBro/src/pages/Dragons.jsx
@@ -1,6 +1,7 @@
 import { useData } from "../backend/FetchData";
 import { Link, useParams } from "react-router-dom";
 import "./Dragons.css";
+import { Spacer } from "./Spacer";
 
 export function Dragons() {
   const dragons = useData("dragons");
@@ -15,11 +16,22 @@ export function Dragons() {
     dragons && (
       <>
         <div className="dragonsListAddBar">
-          <h2 className="dragonsList">Dragons List</h2>
+          <h1 className="dragonsList">Dragons</h1>
+          <Spacer />
+
+          <p className="descriptionText">
+            A dragon is a fire-breathing mythological animal, a winged, flying,
+            scaly creature. In many European legends, brave knights battle evil
+            dragons. The dragon shows up in the mythologies of many different
+            cultures, usually breathing fire and resembling a giant reptile or
+            serpent with wings.
+          </p>
+          <Spacer />
+
           <button className="addDragon">
-            <Link to={"/addDragons"} className="link">
-              ADD DRAGON
-            </Link>
+            {/* <Link to={"/addDragons"} className="link"> */}
+            ADD DRAGON
+            {/* </Link> */}
           </button>
         </div>
 
@@ -34,9 +46,9 @@ export function Dragons() {
                 <p>Habitat: {item.habitat}</p>
                 <div className="btn">
                   <button className="delete">
-                    <Link to={"/delete"} className="orangeLink">
-                      Delete
-                    </Link>
+                    {/* <Link to={"/delete"} className="orangeLink"> */}
+                    Delete
+                    {/* </Link> */}
                   </button>
                 </div>
               </div>
