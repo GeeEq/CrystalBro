4579a5d0bb280d80313f4c8e8bccc78f8757e8fc
Gediminas G. <164216744+GeeEq@users.noreply.github.com>
2024-06-25 13:17:49 +0300

    home links z-index and styling done

--- CrystalBro/src/pages/Body.jsx
+++ CrystalBro/src/pages/Body.jsx
@@ -8,6 +8,18 @@ import { Spacer } from "./Spacer";
 export function BodyPage() {
   return (
     <>
+      <section className="bodyDescText">
+        <h2>Imagination</h2>
+        <p className="textDescr">
+          Imagination is the production of sensations, feelings and thoughts
+          informing oneself.These experiences can be re-creations of past
+          experiences, such as vivid memories with imagined changes, or
+          completely invented and possibly fantastic scenes. Imagination helps
+          apply knowledge to solve problems and is fundamental to integrating
+          experience and the learning process. a way of building theory, it is
+          called "disciplined imagination".
+        </p>
+      </section>
       <div className="bodyLinks">
         <div className="display">
           <div className="alienAlign">
